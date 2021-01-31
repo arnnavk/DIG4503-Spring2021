334239847677b2c494368ee2dd55aa3e9fb45e91
arnnavk <arnavbansal05@gmail.com>
2021-01-31 20:52:57 +0530

    Pokemon Commit

---
+++ Node Project 2/Fetch.js
@@ -0,0 +1,14 @@
+export class Fetch {
+    constructor(pokemon, color)
+    {
+        this.pokemon = pokemon;
+        this.color = color;
+    }
+    Fetch()
+    {
+        
+    }
+    speak() {
+        console.log(this.pokemon, this.color);
+    }
+}
---
+++ Node Project 2/index.js
@@ -0,0 +1,3 @@
+import { Fetch } from './Fetch.js';
+const c = new Fetch("ditto", "red");
+const d = new Fetch("Arnav", "blue");
