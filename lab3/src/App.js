56c4d0e9ec8980dcf30313fd49f1030ff236a206
arnnavk <arnavbansal05@gmail.com>
2021-02-10 20:37:01 +0530

    LAB3 COMMIT

---
+++ lab3/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Homepage from './Homepage';
+function App() {
+  return (
+    <div>
+      <Homepage firstName ="Arnav" />
+    </div>
+  );
+}
+
+export default App;
---
+++ lab3/src/Homepage.js
@@ -0,0 +1,8 @@
+import React from 'React';
+class Homepage extends React.Component {
+    render() {
+      return <h1>{this.props.firstName}</h1>;
+    }
+  }
+
+  export default Homepage;
