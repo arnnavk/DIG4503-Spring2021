a3802f0e2ab102641ded9d5e5e8cd2d17c74dad6
arnnavk <arnavbansal05@gmail.com>
2021-02-14 19:43:32 +0530

    LAB4 COMMIT

---
+++ lab4/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Market from './components/Market';
+
+class App extends React.Component{
+  render() {
+    return (<h1>{this.props}</h1>);
+  }
+}
+
+export default App;
