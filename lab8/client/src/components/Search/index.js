87f67b2be11bb6eb685b1b51ffbb2c61cd4f4737
arnnavk <arnavbansal05@gmail.com>
2021-03-22 18:06:26 +0530

    Lab08 Commit

---
+++ lab8/client/src/components/Search/index.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Axios from 'axios';
+
+function Search() {
+  
+  const [search, setSearch] = useState("");
+
+  function searching() {
+
+    Axios.get('https://localhost:45030/people/' + search)
+    // And then...
+    .then(function (response) {
+        // Use the 'data'
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log("Not found!");
+    });
+  }
+  
+  return (
+    <div classname="Search">
+        <h1>Search for a name</h1>
+      <input type="text" onChange={(event) => {
+        setSearch(event.target.value);
+      }} />
+      <button onClick={() => searching()}>Search</button>
+    </div>
+  );
+}
+
+export default Search;
